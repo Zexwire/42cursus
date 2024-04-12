9a0413d963f4cf75b0424b6d1f77143684b646d0
Jacob <zxmarcos04@gmail.com>
2024-04-12 17:16:40 +0200

    a

--- libft/ft_lstiter.c
+++ libft/ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: mcarnere <mcarnere@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 21:12:04 by mcarnere          #+#    #+#             */
-/*   Updated: 2024/03/26 21:18:53 by mcarnere         ###   ########.fr       */
+/*   Updated: 2024/03/26 22:09:01 by mcarnere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
 	while (lst)
 	{
-		f(lst->content);
+		(*f)(lst->content);
 		lst = lst->next;
-	}	
+	}
 }
--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: mcarnere <mcarnere@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 21:23:24 by mcarnere          #+#    #+#             */
-/*   Updated: 2024/03/26 21:23:38 by mcarnere         ###   ########.fr       */
+/*   Updated: 2024/03/26 22:07:25 by mcarnere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,25 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list	*res;
+	t_list	*node;
+	void	*content;
 
+	if (!f || !del)
+		return (NULL);
+	res = NULL;
+	while (lst)
+	{
+		content = (*f)(lst->content);
+		node = ft_lstnew(content);
+		if (!node)
+		{
+			(*del)(content);
+			ft_lstclear(&res, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&res, node);
+		lst = lst->next;
+	}
+	return (res);
 }
